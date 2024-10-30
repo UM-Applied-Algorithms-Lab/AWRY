da2d0ee518bf35bcfa47af1a711c8d9b0f6acf5c
Tim Anderson <timanderson.um@gmail.com>
2024-10-30 16:36:11 -0600

    update bwt to implement occurrence function
    okay, this turned out pretty clean! I like how this is structured. I need to check these are correct, so I'll add a todo to double check with fresh eyes

--- src/bwt.rs
+++ src/bwt.rs
@@ -1,27 +1,12 @@
-use alphabet::Alphabet;
-use std::ops::BitAnd;
-use std::ops::BitOr;
-use std::ops::BitXor;
-use std::simd::Simd;
-
 use crate::alphabet::{alphabet_cardinality, ascii_to_encoded, ascii_to_index};
-type SimdVec256 = wide::u64x4;
+use crate::simd_instructions::{AlignedVectorArray, SimdVec256};
+use alphabet::Alphabet;
 
 pub const NUM_NUCLEOTIDE_MILESTONES: usize = 8; //4 nucs, plus N, and $, padded to 8
 pub const NUM_AMINO_MILESTONES: usize = 24; //20 AAs, plus X, and $, padded to 24
 pub const NUM_NUCLEOTIDE_BIT_VECTORS: usize = 3;
 pub const NUM_AMINO_BIT_VECTORS: usize = 5;
 pub const NUM_POSITIONS_PER_BLOCK: usize = 256;
-
-#[repr(align(32))]
-struct AlignedVectorArray {
-    data: [u64; 4],
-}
-impl AlignedVectorArray {
-    fn new() -> Self {
-        AlignedVectorArray { data: [0; 4] }
-    }
-}
 
 #[derive(Clone)]
 pub struct NucleotideBwtBlock {
@@ -37,10 +22,9 @@ pub struct AminoBwtBlock {
 
 impl NucleotideBwtBlock {
     pub fn new() -> Self {
-        let aligned_empty_vector = AlignedVectorArray::new();
         NucleotideBwtBlock {
             milestones: [0; NUM_NUCLEOTIDE_MILESTONES],
-            bit_vectors: [SimdVec256::new(aligned_empty_vector.data); NUM_NUCLEOTIDE_BIT_VECTORS],
+            bit_vectors: [SimdVec256::zero(); NUM_NUCLEOTIDE_BIT_VECTORS],
         }
     }
 
@@ -57,52 +41,33 @@ impl NucleotideBwtBlock {
     }
 
     #[inline]
-    pub fn global_occurrence(&self, local_query_position: usize, letter_idx: u8) -> usize {
+    pub fn global_occurrence(&self, local_query_position: u64, letter_idx: u8) -> u64 {
         let milestone_count = self.get_milestone(letter_idx);
         let vectors = &self.bit_vectors;
-        todo!("get a andnot working");
         let occurrence_vector = match letter_idx {
-            0 => vectors[1].bitand(vectors[2]), //A:    0b110
-            1 => vectors[0].bitand(vectors[2]), //C:    0b101
-            2 => vectors[0].bitand(vectors[1]), //G:    0b011
-            3 => vectors[0].bitand(vectors[1].bitand(vectors[2])), //T:    0b111
-            4 => vectors[0],
+            0 => vectors[1].and(&vectors[2]), //A:    0b110
+            1 => vectors[0].and(&vectors[2]), //C:    0b101
+            2 => vectors[0].and(&vectors[1]), //G:    0b011
+            3 => vectors[2].andnot(&vectors[1].andnot(&vectors[0])), //T:    0b001
+            4 => vectors[2].andnot(&vectors[0].andnot(&vectors[1])), //N:    0b010
             _ => {
                 panic!("illegal letter index given in global occurrence function");
             } //assume every other character is an N, since it's illegal to search for a sentinel
         };
 
+        let popcount = occurrence_vector.masked_popcount(local_query_position);
+
         //generate the position bitmask
-        let mut position_bitmask_vector = AlignedVectorArray::new();
-        let num_fill_bitmask_elements = local_query_position / 64;
-        for element_idx in 0..num_fill_bitmask_elements{
-            position_bitmask_vector.data[element_idx] = !(0 as u64); //all set ones for a 64-bit int.
-        }
-        //for the final element, fill with as many ones as needed
-        position_bitmask_vector.data[num_fill_bitmask_elements] = !(0 as u64) >> (63 - (local_query_position % 64));
-        
-        let position_bitmask: SimdVec256 = SimdVec256::new(position_bitmask_vector.data);
-        //apply the bitmask and count up the ones in the SIMD vector, giving us the local character count.
-        let masked_position_vector = occurrence_vector.bitand(position_bitmask);
-        let masked_position_array = masked_position_vector.to_array();
 
-        //get the popcount
-        let mut popcount = 0;
-        for element_idx in 0..4 {
-            popcount += masked_position_array[element_idx].count_ones();
-        };
-
-        return popcount as usize + milestone_count as usize;
+        return popcount as u64 + milestone_count;
     }
 }
 
-
 impl AminoBwtBlock {
     pub fn new() -> Self {
-        let aligned_empty_vector = AlignedVectorArray::new();
         AminoBwtBlock {
             milestones: [0; NUM_AMINO_MILESTONES],
-            bit_vectors: [SimdVec256::new(aligned_empty_vector.data); NUM_AMINO_BIT_VECTORS],
+            bit_vectors: [SimdVec256::zero(); NUM_AMINO_BIT_VECTORS],
         }
     }
     #[inline]
@@ -116,42 +81,38 @@ impl AminoBwtBlock {
         return self.milestones[letter_idx as usize];
     }
     #[inline]
-    pub fn global_occurrence(&self, local_query_position: usize, letter_idx: u8) -> usize {
+    pub fn global_occurrence(&self, local_query_position: u64, letter_idx: u8) -> usize {
         let milestone_count = self.get_milestone(letter_idx);
-        let vectors = &self.bit_vectors;
-        todo!("get a andnot working");
-        todo!("replace occurrence calc ")
+        let vecs = &self.bit_vectors;
         let occurrence_vector = match letter_idx {
-            0 => vectors[1].bitand(vectors[2]), //A:    0b110
-            1 => vectors[0].bitand(vectors[2]), //C:    0b101
-            2 => vectors[0].bitand(vectors[1]), //G:    0b011
-            3 => vectors[0].bitand(vectors[1].bitand(vectors[2])), //T:    0b111
-            4 => vectors[0],
+            0 => vecs[3].and(&vecs[4].andnot(&vecs[2])), //A:    0b01100
+            1 => vecs[3].andnot(&vecs[2]).and(&vecs[1].andnot(&vecs[0])), //C:    0b10111
+            2 => vecs[1].and(&vecs[4].andnot(&vecs[0])), //D:    0b00011
+            3 => vecs[4].andnot(&vecs[2].and(&vecs[1])), //E: 0b00110
+            4 => vecs[0].andnot(&vecs[3]).and(&vecs[2].and(&vecs[1])), //F:    0b11110
+            5 => vecs[2].andnot(&vecs[0].andnot(&vecs[4])), //G:    0b11010
+            6 => vecs[2].andnot(&vecs[3]).and(&vecs[1].and(&vecs[0])), //H: 0b11011
+            7 => vecs[2].andnot(&vecs[1].andnot(&vecs[4])), //I:    0b11001
+            8 => vecs[3].andnot(&vecs[1].andnot(&vecs[4])), //K:    0b10101
+            9 => vecs[1].andnot(&vecs[0].andnot(&vecs[4])), //L:    0b11100
+            10 => vecs[1].andnot(&vecs[3]).and(&vecs[2].and(&vecs[0])), //M:    0b11101
+            11 => vecs[0].or(&vecs[1]).andnot(&vecs[2].andnot(&vecs[3])), //N:    0b01000
+            12 => vecs[3].and(&vecs[4].andnot(&vecs[0])), //P:    0b01001,
+            13 => vecs[3].or(&vecs[1]).andnot(&vecs[0].andnot(&vecs[2])), //Q:    0b00100
+            14 => vecs[3].andnot(&vecs[2].andnot(&vecs[4])), //R:    0b10011
+            15 => vecs[3].and(&vecs[4].andnot(&vecs[1])), //S:    0b01010
+            16 => vecs[2].and(&vecs[4].andnot(&vecs[0])), //T:    0b00101
+            17 => vecs[3].andnot(&vecs[0].andnot(&vecs[4])), //V:    0b10110
+            18 => vecs[3].or(&vecs[2]).andnot(&vecs[1].andnot(&vecs[0])), //W:    0b00001
+            19 => vecs[0].or(&vecs[2]).andnot(&vecs[3].andnot(&vecs[1])), //Y:    0b00010
+            20 => vecs[3].and(&vecs[2]).and(&vecs[1].and(&vecs[0])), //Ambiguity character:  0b11111
+            // 0b00000 is sentinel, but since you can't search for sentinels, it is not included here.
             _ => {
                 panic!("illegal letter index given in global occurrence function");
-            } //assume every other character is an N, since it's illegal to search for a sentinel
+            }
         };
 
-        //generate the position bitmask
-        let mut position_bitmask_vector = AlignedVectorArray::new();
-        let num_fill_bitmask_elements = local_query_position / 64;
-        for element_idx in 0..num_fill_bitmask_elements{
-            position_bitmask_vector.data[element_idx] = !(0 as u64); //all set ones for a 64-bit int.
-        }
-        //for the final element, fill with as many ones as needed
-        position_bitmask_vector.data[num_fill_bitmask_elements] = !(0 as u64) >> (63 - (local_query_position % 64));
-        
-        let position_bitmask: SimdVec256 = SimdVec256::new(position_bitmask_vector.data);
-        //apply the bitmask and count up the ones in the SIMD vector, giving us the local character count.
-        let masked_position_vector = occurrence_vector.bitand(position_bitmask);
-        let masked_position_array = masked_position_vector.to_array();
-
-        //get the popcount
-        let mut popcount = 0;
-        for element_idx in 0..4 {
-            popcount += masked_position_array[element_idx].count_ones();
-        };
-
+        let popcount = occurrence_vector.masked_popcount(local_query_position);
         return popcount as usize + milestone_count as usize;
     }
 }
