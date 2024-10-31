3d760b4f98bf91b9599cc320ae6d82cb66d101c0
Tim Anderson <timanderson.um@gmail.com>
2024-10-31 17:45:43 -0600

    update alphabet and symbols for better rustiness
    this is structured much more cleanly, and is something I'd be proud to show others.

--- src/alphabet.rs
+++ src/alphabet.rs
@@ -1,3 +1,19 @@
+enum SymbolEncoding {
+    Ascii(char),
+    Index(u8),
+    BitVector(u8),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum SymbolAlphabet {
+    Nucleotide,
+    Amino,
+}
+
+struct Symbol {
+    alphabet: SymbolAlphabet,
+    encoding: SymbolEncoding,
+}
 enum BwtNucleotideSymbol {
     Ascii(char),
     Index(u8),
@@ -10,90 +26,274 @@ enum BwtAminoSymbol {
     BitVector(u8),
 }
 
-pub enum Alphabet {
-    Nucleotide,
-    Amino,
-}
 
-pub fn alphabet_cardinality(alphabet: &Alphabet)-> u8 {
+pub fn alphabet_cardinality(alphabet: &SymbolAlphabet) -> u8 {
     match alphabet {
-        Alphabet::Nucleotide => 6,
-        Alphabet::Amino => 22,
+        SymbolAlphabet::Nucleotide => 6,
+        SymbolAlphabet::Amino => 22,
     }
 }
 
-pub fn ascii_to_index(symbol: &char, alphabet: &Alphabet) -> u8 {
-    match alphabet {
-        Alphabet::Nucleotide => match symbol {
-            '#' | '$' => 0,
-            'A' | 'a' => 1,
-            'C' | 'c' => 2,
-            'G' | 'g' => 3,
-            'T' | 't' | 'U' | 'u' => 5,
-            _ => 4,
-        },
-        Alphabet::Amino => match symbol {
-            '#' | '$' => 0,
-            'A' | 'a' => 1,
-            'C' | 'c' => 2,
-            'D' | 'd' => 3,
-            'E' | 'e' => 4,
-            'F' | 'f' => 5,
-            'G' | 'g' => 6,
-            'H' | 'h' => 7,
-            'I' | 'i' => 8,
-            'K' | 'k' => 9,
-            'L' | 'l' => 10,
-            'M' | 'm' => 11,
-            'N' | 'n' => 12,
-            'P' | 'p' => 13,
-            'Q' | 'q' => 14,
-            'R' | 'r' => 15,
-            'S' | 's' => 16,
-            'T' | 't' => 17,
-            'V' | 'v' => 18,
-            'W' | 'w' => 19,
-            'Y' | 'y' => 21,
-
-            _ => 20,
-        },
+impl Symbol {
+    pub fn new_ascii(alphabet: SymbolAlphabet, ascii: char) -> Symbol {
+        Symbol {
+            alphabet,
+            encoding: SymbolEncoding::Ascii(ascii),
+        }
     }
-}
+    pub fn new_index(alphabet: SymbolAlphabet, index: u8) -> Symbol {
+        Symbol {
+            alphabet,
+            encoding: SymbolEncoding::Index(index),
+        }
+    }
+    pub fn new_bit_vector(alphabet: SymbolAlphabet, bit_vector: u8) -> Symbol {
+        Symbol {
+            alphabet,
+            encoding: SymbolEncoding::BitVector(bit_vector),
+        }
+    }
 
-pub fn ascii_to_encoded(symbol: &char, alphabet: &Alphabet) -> u8 {
-    match alphabet {
-        Alphabet::Nucleotide => match symbol {
-            '#' | '$' => 0b100,
-            'A' | 'a' => 0b110,
-            'C' | 'c' => 0b101,
-            'G' | 'g' => 0b011,
-            'T' | 't' | 'U' | 'u' => 0b010,
+    pub fn to_index(&self) -> Symbol {
+        match self.alphabet {
+            SymbolAlphabet::Amino => Symbol {
+                alphabet: self.alphabet,
+                encoding: SymbolEncoding::Index(match self.encoding {
+                    SymbolEncoding::Ascii(encoding) => match encoding {
+                        '#' | '$' => 0,
+                        'A' | 'a' => 1,
+                        'C' | 'c' => 2,
+                        'D' | 'd' => 3,
+                        'E' | 'e' => 4,
+                        'F' | 'f' => 5,
+                        'G' | 'g' => 6,
+                        'H' | 'h' => 7,
+                        'I' | 'i' => 8,
+                        'K' | 'k' => 9,
+                        'L' | 'l' => 10,
+                        'M' | 'm' => 11,
+                        'N' | 'n' => 12,
+                        'P' | 'p' => 13,
+                        'Q' | 'q' => 14,
+                        'R' | 'r' => 15,
+                        'S' | 's' => 16,
+                        'T' | 't' => 17,
+                        'V' | 'v' => 18,
+                        'W' | 'w' => 19,
+                        'Y' | 'y' => 21,
+                        _ => 20, //ambiguity character
+                    },
+                    SymbolEncoding::Index(encoding) => encoding,
+                    SymbolEncoding::BitVector(encoding) => match encoding {
+                        0b00000 => 0,  //$
+                        0b01100 => 1,  //A
+                        0b10111 => 2,  //C
+                        0b00011 => 3,  //D
+                        0b00110 => 4,  //E
+                        0b11110 => 5,  //F
+                        0b11010 => 6,  //G
+                        0b11011 => 7,  //H
+                        0b11001 => 8,  //I
+                        0b10101 => 9,  //K
+                        0b11100 => 10, //L
+                        0b11101 => 11, //M
+                        0b01000 => 12, //N
+                        0b01001 => 13, //P
+                        0b00100 => 14, //Q
+                        0b10011 => 15, //R
+                        0b01010 => 16, //S
+                        0b00101 => 17, //T
+                        0b10110 => 18, //V
+                        0b00001 => 19, //W
+                        0b00010 => 21, //Y
+                        _ => 20,       // ambiguity char X
+                    },
+                }),
+            },
+            SymbolAlphabet::Nucleotide => Symbol {
+                alphabet: self.alphabet,
+                encoding: SymbolEncoding::Index(match self.encoding {
+                    SymbolEncoding::Ascii(encoding) => match encoding {
+                        '#' | '$' => 0,
+                        'A' | 'a' => 1,
+                        'C' | 'c' => 2,
+                        'G' | 'g' => 3,
+                        'T' | 't' | 'U' | 'u' => 5,
+                        _ => 4, //ambiguity char N
+                    },
+                    SymbolEncoding::Index(encoding) => encoding,
+                    SymbolEncoding::BitVector(encoding) => match encoding {
+                        0b100 => 0, //$
+                        0b110 => 1, //A
+                        0b101 => 2, //C
+                        0b011 => 3, //G
+                        0b001 => 5, //T
+                        _ => 4,     //N
+                    },
+                }),
+            },
+        }
+    }
 
-            _ => 0b000,
-        },
-        Alphabet::Amino => match symbol {
-            '#' | '$' => 0b00000,
-            'A' | 'a' => 0b01100,
-            'C' | 'c' => 0b10111,
-            'D' | 'd' => 0b00011,
-            'E' | 'e' => 0b00110,
-            'F' | 'f' => 0b11110,
-            'G' | 'g' => 0b11010,
-            'H' | 'h' => 0b11011,
-            'I' | 'i' => 0b11001,
-            'K' | 'k' => 0b10101,
-            'L' | 'l' => 0b11100,
-            'M' | 'm' => 0b11101,
-            'N' | 'n' => 0b01000,
-            'P' | 'p' => 0b01001,
-            'Q' | 'q' => 0b00100,
-            'R' | 'r' => 0b10011,
-            'S' | 's' => 0b01010,
-            'T' | 't' => 0b00101,
-            'V' | 'v' => 0b10110,
-            'W' | 'w' => 0b00001,
-            'Y' | 'y' => 0b00010,
-            _ => 0b11111,
-        },
+    pub fn to_bit_vector(&self) -> Symbol {
+        match self.alphabet {
+            SymbolAlphabet::Amino => Symbol {
+                alphabet: self.alphabet,
+                encoding: SymbolEncoding::BitVector(match self.encoding {
+                    SymbolEncoding::Ascii(encoding) => match encoding {
+                        '$' | '#' => 0b00000,
+                        'a' | 'A' => 0b01100,
+                        'c' | 'C' => 0b10111,
+                        'd' | 'D' => 0b00011,
+                        'e' | 'E' => 0b00110,
+                        'f' | 'F' => 0b11110,
+                        'g' | 'G' => 0b11010,
+                        'h' | 'H' => 0b11011,
+                        'i' | 'I' => 0b11001,
+                        'k' | 'K' => 0b10101,
+                        'l' | 'L' => 0b11100,
+                        'm' | 'M' => 0b11101,
+                        'n' | 'N' => 0b01000,
+                        'p' | 'P' => 0b01001,
+                        'q' | 'Q' => 0b00100,
+                        'r' | 'R' => 0b10011,
+                        's' | 'S' => 0b01010,
+                        't' | 'T' => 0b00101,
+                        'v' | 'V' => 0b10110,
+                        'w' | 'W' => 0b00001,
+                        'y' | 'Y' => 0b00010,
+                        _ => 0b11111, //ambiguity char X
+                    },
+                    SymbolEncoding::Index(encoding) => match encoding {
+                        0 => 0b00000,
+                        1 => 0b01100,
+                        2 => 0b10111,
+                        3 => 0b00011,
+                        4 => 0b00110,
+                        5 => 0b11110,
+                        6 => 0b11010,
+                        7 => 0b11011,
+                        8 => 0b11001,
+                        9 => 0b10101,
+                        10 => 0b11100,
+                        11 => 0b11101,
+                        12 => 0b01000,
+                        13 => 0b01001,
+                        14 => 0b00100,
+                        15 => 0b10011,
+                        16 => 0b01010,
+                        17 => 0b00101,
+                        18 => 0b10110,
+                        19 => 0b00001,
+                        21 => 0b00010,
+                        _ => 0b11111, //ambiguity char x
+                    },
+                    SymbolEncoding::BitVector(encoding) => encoding,
+                }),
+            },
+            SymbolAlphabet::Nucleotide => Symbol {
+                alphabet: self.alphabet,
+                encoding: SymbolEncoding::BitVector(match self.encoding {
+                    SymbolEncoding::Ascii(encoding) => match encoding {
+                        '#' | '$' => 0b100,
+                        'A' | 'a' => 0b110,
+                        'C' | 'c' => 0b101,
+                        'G' | 'g' => 0b011,
+                        'T' | 't' | 'U' | 'u' => 0b001,
+                        _ => 0b010, //ambiguity char N
+                    },
+                    SymbolEncoding::Index(encoding) => match encoding {
+                        0 => 0b100,
+                        1 => 0b110,
+                        2 => 0b101,
+                        3 => 0b011,
+                        5 => 0b010,
+                        _ => 0b000, //4 is ambiguity character,
+                    },
+                    SymbolEncoding::BitVector(encoding) => encoding,
+                }),
+            },
+        }
+    }
+
+    pub fn to_ascii(&self) -> Symbol {
+        match self.alphabet {
+            SymbolAlphabet::Amino => Symbol {
+                alphabet: self.alphabet,
+                encoding: SymbolEncoding::Ascii(match self.encoding {
+                    SymbolEncoding::Ascii(encoding) => encoding,
+                    SymbolEncoding::Index(encoding) => match encoding {
+                        0 => '$',
+                        1 => 'A',
+                        2 => 'C',
+                        3 => 'D',
+                        4 => 'E',
+                        5 => 'F',
+                        6 => 'G',
+                        7 => 'H',
+                        8 => 'I',
+                        9 => 'K',
+                        10 => 'L',
+                        11 => 'M',
+                        12 => 'N',
+                        13 => 'P',
+                        14 => 'Q',
+                        15 => 'R',
+                        16 => 'S',
+                        17 => 'T',
+                        18 => 'V',
+                        19 => 'W',
+                        21 => 'Y',
+                        _ => 'X', //20, is ambiguity
+                    },
+                    SymbolEncoding::BitVector(encoding) => match encoding {
+                        0b00000 => '$',
+                        0b01100 => 'A',
+                        0b10111 => 'C',
+                        0b00011 => 'D',
+                        0b00110 => 'E',
+                        0b11110 => 'F',
+                        0b11010 => 'G',
+                        0b11011 => 'H',
+                        0b11001 => 'I',
+                        0b10101 => 'K',
+                        0b11100 => 'L',
+                        0b11101 => 'M',
+                        0b01000 => 'N',
+                        0b01001 => 'P',
+                        0b00100 => 'Q',
+                        0b10011 => 'R',
+                        0b01010 => 'S',
+                        0b00101 => 'T',
+                        0b10110 => 'V',
+                        0b00001 => 'W',
+                        0b00010 => 'Y',
+                        _ => 'X',
+                    },
+                }),
+            },
+            SymbolAlphabet::Nucleotide => Symbol {
+                alphabet: self.alphabet,
+                encoding: SymbolEncoding::Ascii(match self.encoding {
+                    SymbolEncoding::Ascii(encoding) => encoding,
+                    SymbolEncoding::Index(encoding) => match encoding {
+                        0 => '$',
+                        1 => 'A',
+                        2 => 'C',
+                        3 => 'G',
+                        5 => 'T',
+                        _ => 'N',
+                    },
+                    SymbolEncoding::BitVector(encoding) => match encoding {
+                        0b100 => '$',
+                        0b110 => 'A',
+                        0b101 => 'C',
+                        0b011 => 'G',
+                        0b001 => 'T',
+                        _ => 'N',
+                    },
+                }),
+            },
+        }
     }
 }
