5147f31cc9a1fe8a317035f60ca25c9501adced4
Tim Anderson <timanderson.um@gmail.com>
2024-11-01 14:20:05 -0600

    update bwt to use symbols as query type

--- src/bwt.rs
+++ src/bwt.rs
@@ -1,4 +1,4 @@
-use crate::simd_instructions::SimdVec256;
+use crate::{alphabet::Symbol, simd_instructions::SimdVec256};
 
 pub const NUM_NUCLEOTIDE_MILESTONES: usize = 8; //4 nucs, plus N, and $, padded to 8
 pub const NUM_AMINO_MILESTONES: usize = 24; //20 AAs, plus X, and $, padded to 24
@@ -34,15 +34,15 @@ impl NucleotideBwtBlock {
     }
 
     #[inline]
-    pub fn get_milestone(&self, letter_idx: u8) -> u64 {
-        return self.milestones[letter_idx as usize];
+    pub fn get_milestone(&self, symbol: &Symbol) -> u64 {
+        return self.milestones[symbol.index() as usize];
     }
 
     #[inline]
-    pub fn global_occurrence(&self, local_query_position: usize, letter_idx: u8) -> u64 {
-        let milestone_count = self.get_milestone(letter_idx);
+    pub fn global_occurrence(&self, local_query_position: usize, symbol: &Symbol) -> u64 {
+        let milestone_count = self.get_milestone(&symbol);
         let vectors = &self.bit_vectors;
-        let occurrence_vector = match letter_idx {
+        let occurrence_vector = match &symbol.index() {
             0 => vectors[2].and(&vectors[1]), //A:    0b110
             1 => vectors[2].and(&vectors[0]), //C:    0b101
             2 => vectors[1].and(&vectors[0]), //G:    0b011
@@ -75,14 +75,14 @@ impl AminoBwtBlock {
         }
     }
     #[inline]
-    pub fn get_milestone(&self, letter_idx: u8) -> u64 {
-        return self.milestones[letter_idx as usize];
+    pub fn get_milestone(&self, symbol: &Symbol) -> u64 {
+        return self.milestones[symbol.index() as usize];
     }
     #[inline]
-    pub fn global_occurrence(&self, local_query_position: usize, letter_idx: u8) -> u64 {
-        let milestone_count = self.get_milestone(letter_idx);
+    pub fn global_occurrence(&self, local_query_position: usize, symbol: &Symbol) -> u64 {
+        let milestone_count = self.get_milestone(symbol);
         let vecs = &self.bit_vectors;
-        let occurrence_vector = match letter_idx {
+        let occurrence_vector = match symbol.index() {
             0 => vecs[3].and(&vecs[4].andnot(&vecs[2])), //A:    0b01100
             1 => vecs[3].andnot(&vecs[2]).and(&vecs[1].andnot(&vecs[0])), //C:    0b10111
             2 => vecs[1].and(&vecs[4].andnot(&vecs[0])), //D:    0b00011
@@ -124,13 +124,14 @@ impl Bwt {
     /// sets a single symbol at the given position in the bwt bit vectors.
     /// this function is meant to be run for every position in the bwt
     /// as a part of BWT data creation
-    pub fn set_symbol_at(&self, btw_position: usize, mut encoded_symbol: u8) {
+    pub fn set_symbol_at(&self, btw_position: usize, symbol: &Symbol) {
         //find the block, byte, and bit of the data we're setting
         let position_block_idx: usize = (btw_position / NUM_POSITIONS_PER_BLOCK) as usize;
         let position_in_block = btw_position % NUM_POSITIONS_PER_BLOCK;
 
         //create a bitmask, we'll use this to set the bit with an OR operation
         let vector_bitmask = SimdVec256::as_one_hot(position_in_block);
+        let mut encoded_symbol = symbol.bit_vector();
 
         let mut bit_vector_idx = 0;
         while encoded_symbol != 0 {
@@ -161,15 +162,13 @@ impl Bwt {
             Bwt::Amino(vec) => vec[block_idx].milestones[letter_idx as usize],
         };
     }
-    pub fn global_occurrence(&self, pointer_global_position: usize, letter_idx: u8) -> u64 {
+    pub fn global_occurrence(&self, pointer_global_position: usize, symbol: &Symbol) -> u64 {
         let block_idx: usize = pointer_global_position / NUM_POSITIONS_PER_BLOCK;
         let local_query_position: usize = pointer_global_position % NUM_POSITIONS_PER_BLOCK;
 
         match self {
-            Bwt::Nucleotide(vec) => {
-                vec[block_idx].global_occurrence(local_query_position, letter_idx)
-            }
-            Bwt::Amino(vec) => vec[block_idx].global_occurrence(local_query_position, letter_idx),
+            Bwt::Nucleotide(vec) => vec[block_idx].global_occurrence(local_query_position, symbol),
+            Bwt::Amino(vec) => vec[block_idx].global_occurrence(local_query_position, symbol),
         }
     }
 }
