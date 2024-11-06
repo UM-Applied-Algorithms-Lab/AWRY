74be5db0eadf4a2ba1172053bd1cabb4d7d63bf8
Tim Anderson <timanderson.um@gmail.com>
2024-11-06 13:33:22 -0700

    add code for handling compressed suffix arrays

---
+++ src/compressed_suffix_array.rs
@@ -0,0 +1,62 @@
+
+struct CompressedSuffixArray {
+    data: Vec<u64>,
+    length: usize,
+    suffix_array_compression_ratio: usize,
+    bits_per_element: u64,
+}
+
+impl CompressedSuffixArray {
+    pub fn new(length: usize, suffix_array_compression_ratio: usize) -> Self {
+        //finds how many bits would be required to store any integer in the range 0 to length-1
+        let mut num_bits_required: u64 = 0;
+        let mut maximum_sa_value = length - 1;
+        while maximum_sa_value != 0 {
+            num_bits_required += 1;
+            maximum_sa_value >>= 1;
+        }
+
+        CompressedSuffixArray {
+            data: vec![0; length],
+            length,
+            suffix_array_compression_ratio,
+            bits_per_element: num_bits_required,
+        }
+    }
+
+    pub fn set_value(&mut self, value: u64, position: usize) {
+        let word_position = position / 64;
+        let bit_position = position % 64;
+
+        //create bitmasks to erase value here, and generate the values to write
+        let first_word_bitmask = !(!0u64 << bit_position);
+        let second_word_bitmask = !0u64 >> (64 - bit_position);
+        let first_word_value = value << bit_position;
+        let second_word_value = value >> (64 - bit_position);
+
+        //apply the bitmasks and set the values
+        self.data[word_position] &= first_word_bitmask;
+        self.data[word_position + 1] &= second_word_bitmask;
+        self.data[word_position] |= first_word_value;
+        self.data[word_position + 1] |= second_word_value;
+    }
+
+    pub fn get_value(&self, position: usize) -> Option<u64> {
+        if position % self.suffix_array_compression_ratio == 0 {
+            Some(self.reconstruct_value(position))
+        } else {
+            None
+        }
+    }
+
+    ///reconstructs the value stored in the given element position in the compressed suffix array.
+    /// Note that this position does not represent the uncompressed SA position, but instead the
+    /// ith index in the compressed SA.
+    fn reconstruct_value(&self, position: usize) -> u64 {
+        let word_position = position / 64;
+        let bit_position = position % 64;
+
+        return self.data[word_position] >> bit_position
+            | (self.data[word_position + 1] >> (64 - bit_position));
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,31 @@
-pub mod simd_instructions;
 pub mod alphabet;
 pub mod bwt;
 pub mod fm_index;
-pub mod search;
 pub mod occurrence;
-use std::intrinsics;
-use sufr;
-
+pub mod search;
+pub mod simd_instructions;
+pub mod compressed_suffix_array;
 //todo: how am I going to square the Suffix Array and the read collection?
 //how to get the suffix array data out (use the sufr library its self)
 //todo: count function, returns range, method on range to get len
 //todo: set up hashmap for cannonical kmers
 //todo: allow for adding 0-weighted references (linear walk through fastas)
 
-fn create_sufr_suffix_array()-> Result<()>{
+fn create_sufr_suffix_array() -> Result<()> {}
 
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_ceil() {
+        assert_eq!((100 as usize).div_ceil(3), 34);
+        assert_eq!((101 as usize).div_ceil(3), 34);
+        assert_eq!((102 as usize).div_ceil(3), 34);
+        assert_eq!((103 as usize).div_ceil(3), 35);
+    }
+
+    #[test]
+    fn ascii_value_to_char(){
+        assert_eq!(64 as char, 'A');
+    }
 }
