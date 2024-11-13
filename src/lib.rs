7e961b8ee2a7fe4269b987f4169b7d3a784711e9
Tim Anderson <timanderson.um@gmail.com>
2024-11-13 16:25:56 -0700

    add kmer_lookup_table to lib.rs, cleanup reminder comments

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,9 @@ pub mod bwt;
 pub mod compressed_suffix_array;
 pub mod fm_index;
 pub mod fm_index_file;
+pub mod kmer_lookup_table;
 pub mod search;
 pub mod simd_instructions;
-//todo: how am I going to square the Suffix Array and the read collection?
-//how to get the suffix array data out (use the sufr library its self)
-//todo: count function, returns range, method on range to get len
-//todo: set up hashmap for cannonical kmers
-//todo: allow for adding 0-weighted references (linear walk through fastas)
-
-fn create_sufr_suffix_array() -> Result<()> {}
 
 #[cfg(test)]
 mod tests {
