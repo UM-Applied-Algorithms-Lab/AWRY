fc766448319d409eac54a18d587809fdf33fb871
Tim Anderson <timanderson.um@gmail.com>
2024-11-14 17:37:01 -0700

    fix bugs with bitcompressing the suffix array values and reading them out, add tests

--- src/compressed_suffix_array.rs
+++ src/compressed_suffix_array.rs
@@ -13,18 +13,12 @@ pub struct CompressedSuffixArray {
 impl CompressedSuffixArray {
     /// Allocates space for a new Compressed Suffix Array, given the total uncompressed length and a compression ratio.
     pub fn new(length: usize, suffix_array_compression_ratio: u64) -> Self {
-        //finds how many bits would be required to store any integer in the range 0 to length-1
-        let mut num_bits_required: u64 = 0;
-        let mut maximum_sa_value = length - 1;
-        while maximum_sa_value != 0 {
-            num_bits_required += 1;
-            maximum_sa_value >>= 1;
-        }
+        let bits_per_element = 64 - (length - 1).leading_zeros() as u64;
 
         CompressedSuffixArray {
             data: vec![0; length],
             suffix_array_compression_ratio,
-            bits_per_element: num_bits_required,
+            bits_per_element,
         }
     }
 
@@ -39,20 +33,16 @@ impl CompressedSuffixArray {
     /// sets the value in the compressed suffix array.
     /// NOTE! the position is the compressed position, not the position in the full SA.
     pub fn set_value(&mut self, value: u64, position: usize) {
-        let word_position = position / 64;
-        let bit_position = position % 64;
+        let word_position = (position * self.bits_per_element as usize) / 64;
+        let bit_position = (position * self.bits_per_element as usize) % 64;
 
         //create bitmasks to erase value here, and generate the values to write
-        let first_word_bitmask = !(!0u64 << bit_position);
-        let second_word_bitmask = !0u64 >> (64 - bit_position);
-        let first_word_value = value << bit_position;
-        let second_word_value = value >> (64 - bit_position);
 
-        //apply the bitmasks and set the values
-        self.data[word_position] &= first_word_bitmask;
-        self.data[word_position + 1] &= second_word_bitmask;
-        self.data[word_position] |= first_word_value;
-        self.data[word_position + 1] |= second_word_value;
+        self.data[word_position] |= value << bit_position;
+        self.data[word_position + 1] |= match value.checked_shr(64 - bit_position as u32) {
+            Some(val) => val,
+            None => 0,
+        };
     }
 
     pub fn get_value(&self, position: usize) -> Option<u64> {
@@ -67,18 +57,101 @@ impl CompressedSuffixArray {
     /// Note that this position does not represent the uncompressed SA position, but instead the
     /// ith index in the compressed SA.
     fn reconstruct_value(&self, position: usize) -> u64 {
-        let word_position = position / 64;
-        let bit_position = position % 64;
-
-        let unmasked_value = self.data[word_position] >> bit_position
-            | (self.data[word_position + 1] >> (64 - bit_position));
+        let word_position = (position * self.bits_per_element as usize) / 64;
+        let bit_position = (position * self.bits_per_element as usize) % 64;
         let bitmask = (1 << self.bits_per_element) - 1;
+        let first_word = (self.data[word_position] & (bitmask << bit_position)) >> bit_position;
+        let second_word = match self.data[word_position + 1].checked_shr(64 - bit_position as u32) {
+            Some(val) => val & (bitmask >> 64 - bit_position),
+            None => 0,
+        };
+        let unmasked_value = first_word | second_word;
 
-        return unmasked_value & bitmask;
+        return unmasked_value;
     }
 
     /// Returns true if the given position is sampled in the compressed suffix array.
     pub fn position_is_sampled(&self, unsampled_position: u64) -> bool {
         return unsampled_position % self.suffix_array_compression_ratio == 0;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::CompressedSuffixArray;
+
+    #[test]
+    fn check_compressed_suffix_array() -> anyhow::Result<()> {
+        let sa_len = 256;
+        let sa_values: Vec<u64> = (0..sa_len).collect();
+        
+        for compression_ratio in 1..16 {
+            let compressed_length = (sa_len / compression_ratio) as usize;
+            let mut csa = CompressedSuffixArray::new(sa_len as usize, compression_ratio);
+
+            assert_eq!(
+                compression_ratio,
+                csa.compression_ratio(),
+                "compression ratio did not match input"
+            );
+
+            //set the values
+            for sa_value_idx in 0..compressed_length{
+                csa.set_value(
+                    sa_values[sa_value_idx * compression_ratio as usize],
+                    sa_value_idx,
+                );
+            }
+
+            //check the compressed suffix array values
+            for sa_value_idx in 0..compressed_length{
+                let expected_value = sa_values[sa_value_idx * compression_ratio as usize];
+                let value_from_csa = csa.reconstruct_value(sa_value_idx);
+                assert_eq!(
+                    value_from_csa,
+                    expected_value,
+                    "sa_idx {} failed for csr {}, supposed to be {}, found {}. bits per is {}",
+                    sa_value_idx,
+                    compression_ratio,
+                    expected_value,
+                    value_from_csa,
+                    csa.bits_per_element
+                );
+            }
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn check_bits_per_element() -> anyhow::Result<()> {
+        for (length, expected_bits) in [
+            (15, 4),
+            (16, 4),
+            (17, 5),
+            (31, 5),
+            (32, 5),
+            (33, 6),
+            (1022, 10),
+            (1023, 10),
+            (1024, 10),
+            (1025, 11),
+            (65535, 16),
+            (65536, 16),
+            (65537, 17),
+            (2usize.pow(31) - 1, 31),
+            (2usize.pow(31), 31),
+            (2usize.pow(31) + 1, 32),
+        ] {
+            let csa = CompressedSuffixArray::new(length, 8);
+
+            assert_eq!(
+                expected_bits, csa.bits_per_element,
+                "bits did not match expected for length {}, bits {}",
+                length, expected_bits
+            );
+        }
+
+        Ok(())
     }
 }
